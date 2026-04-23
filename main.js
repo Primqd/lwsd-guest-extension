dd7d60975db688b24b9f22f7877ed74096694424
Primqd <franklinvchen@outlook.com>
2026-04-23 09:26:27 -0700

    remove unused url parameter

--- main.js
+++ main.js
@@ -3,7 +3,6 @@
 
 // automatically send verified request (based off accept code)
 var _request = new XMLHttpRequest();
-var url = 'https://na.network-auth.com/splash/Ce3pkaI.1.620/grant';
 _request.open('HEAD', window.location, true);
 _request.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
 _request.send(null);
